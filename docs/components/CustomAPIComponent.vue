a571ee202eb6fbc20cfb82551ab6fa9acd1b23b7
Michael Troxler <michael.troxler1@swisscom.com>
2026-01-16 16:26:20 +0100

    change openapi file name

--- docs/components/CustomAPIComponent.vue
+++ docs/components/CustomAPIComponent.vue
@@ -11,7 +11,7 @@ import { SwaggerUIBundle, SwaggerUIStandalonePreset } from 'swagger-ui-dist';
 
 onMounted(() => {
   SwaggerUIBundle({
-    url: '/openapi-mobileid',
+    url: '/openapi-mobileid.yaml',
     dom_id: '#swagger-ui',
     presets: [SwaggerUIBundle.presets.apis, SwaggerUIStandalonePreset],
   });
